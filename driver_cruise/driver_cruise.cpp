133d784f567936ce479b04b3d9c0a56c34ac4cfc
zhaozihao <xxxzhaozihao@gmail.com>
2023-03-01 01:50:10 +0800

    add origin file

---
+++ driver_cruise/driver_cruise.cpp
@@ -0,0 +1,391 @@
+﻿/*▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼*\
+█	Description: User Module for CyberCruise							█
+█	作者: 杨辰兮 & ChatGPT												█
+█	联系方式: yangchenxi@sjtu.edu.cn										█
+█	日期: 2023.02.13							    						█
+\*▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲*/
+
+/*▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼*\
+█	贴士:	您可以折叠 #pragma region 和	#pragma endregion 之间的代码		█
+█	这可以使您获得一次性折叠完成的程序块而不是一个函数的能力					█
+\*▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲*/
+
+/*▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼*\
+█	DLL接口部分，您可以跳过这部分不阅读									█
+█	不要修改这个 #pragma region 中的任何代码!								█
+\*▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲*/
+#pragma region <<< 不要修改这个 region 中的任何代码!
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+#include "driver_cruise.h"
+#include "stdio.h"
+#include <ostream>
+#include <fstream>
+
+#include "class_Visualization.h"
+#define PI 3.141592653589793238462643383279
+
+static void userDriverGetParam(float midline[200][2], float yaw, float yawrate, float speed, float acc, float width, int gearbox, float rpm);
+static void userDriverSetParam(float* cmdAcc, float* cmdBrake, float* cmdSteer, int* cmdGear);
+static int InitFuncPt(int index, void* pt);
+
+// Module Entry Point
+extern "C" int driver_cruise(tModInfo * modInfo)
+{
+	memset(modInfo, 0, 10 * sizeof(tModInfo));
+	modInfo[0].name = "driver_cruise";	// name of the module (short).
+	modInfo[0].desc = "user module for CyberCruise";	// Description of the module (can be long).
+	modInfo[0].fctInit = InitFuncPt;			// Init function.
+	modInfo[0].gfId = 0;
+	modInfo[0].index = 0;
+	return 0;
+}
+
+// Module interface initialization.
+static int InitFuncPt(int, void* pt)
+{
+	tUserItf* itf = (tUserItf*)pt;
+	itf->userDriverGetParam = userDriverGetParam;
+	itf->userDriverSetParam = userDriverSetParam;
+	return 0;
+}
+
+//Global variables for vehicle states
+static float _midline[200][2];
+static float _yaw, _yawrate, _speed, _acc, _width, _rpm;
+static int _gearbox;
+
+static void userDriverGetParam(float midline[200][2], float yaw, float yawrate, float speed, float acc, float width, int gearbox, float rpm)
+{
+	for (int i = 0; i < 200; ++i) _midline[i][0] = midline[i][0], _midline[i][1] = midline[i][1];
+	_yaw = yaw;
+	_yawrate = yawrate;
+	_speed = speed;
+	_acc = acc;
+	_width = width;
+	_rpm = rpm;
+	_gearbox = gearbox;
+}
+#pragma endregion >>>
+
+/*▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼*\
+█	上下确界约束函数									 					█
+█	您需要理解它的功能，建议不要修改										█
+\*▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲*/
+#pragma region <<< Boundaries of control	
+double constrain(double lowerBoundary, double upperBoundary, double input)
+{
+	if (input > upperBoundary)
+		return upperBoundary;
+	else if (input < lowerBoundary)
+		return lowerBoundary;
+	else
+		return input;
+}
+#pragma endregion
+
+/*▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼*\
+█	赛道曲率半径计算函数													█
+█	您需要理解它的功能，建议不要修改										█
+\*▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲*/
+#pragma region <<< Radius of curvature
+//		Given three points ahead, outputs a struct circle.				
+//		{radius:[1,1000], sign{-1:left,1:right}							
+typedef struct Circle
+{
+	double r;
+	int sign;
+}circle;
+
+circle getR(float x1, float y1, float x2, float y2, float x3, float y3)
+{
+	double a, b, c, d, e, f;
+	double r, x, y;
+
+	a = 2 * (x2 - x1);
+	b = 2 * (y2 - y1);
+	c = x2 * x2 + y2 * y2 - x1 * x1 - y1 * y1;
+	d = 2 * (x3 - x2);
+	e = 2 * (y3 - y2);
+	f = x3 * x3 + y3 * y3 - x2 * x2 - y2 * y2;
+	x = (b * f - e * c) / (b * d - e * a);
+	y = (d * c - a * f) / (b * d - e * a);
+	r = sqrt((x - x1) * (x - x1) + (y - y1) * (y - y1));
+	x = constrain(-1000.0, 1000.0, x);
+	y = constrain(-1000.0, 1000.0, y);
+	r = constrain(1.0, 500.0, r);
+	int sign = (x > 0) ? 1 : -1;
+	return { r,sign };
+}
+#pragma endregion >>>
+
+/*▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼*\
+█	以下是核心控制程序													█
+█	主要输入: _midline, _speed											█
+█	次要输入: _yaw, _yawrate, _acc, _width, _rpm,	_gearbox			█
+█	主要输出: *cmdAcc, *cmdBrake, *cmdSteer  							█
+█	次要输出: *cmdGear 【本样例中已实现】									█
+█	详细信息请参见用户手册												█
+\*▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲*/
+
+//基于OpenCV的可视化工具，详情请见文档
+cls_VISUAL cls_visual;
+
+/*▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼*\
+█	手动换挡程序															█
+█	可以不用看懂，建议不要修改，除非您是学(Juan)霸(Wang) :P					█
+\*▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲*/
+#pragma region <<< Manual Gear
+const float fGearShift[2][7] = //0 for downshift, 1 for upshift
+{
+	0,92,128,167,208,233,255,
+	0,105,145,182,224,249,272
+};
+void updateGear(int* cmdGear)
+{
+
+	if (_speed > fGearShift[1][_gearbox] && _gearbox < 7) //upshift
+	{
+		*cmdGear = _gearbox + 1;
+	}
+	else if (_speed < fGearShift[0][_gearbox - 1] && _gearbox > 1) //downshift
+	{
+		*cmdGear = _gearbox - 1;
+	}
+	else
+	{
+		*cmdGear = _gearbox;
+	}
+}
+#pragma endregion >>>
+
+/*▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼*\
+█	PID控制器，由ChatGPT生成												█
+█	可选择性修改，需要完全理解												█
+\*▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲*/
+class PIDController
+{
+private:
+	double kp, ki, kd;		// PID控制器的参数
+	double targetValue;		// 目标值
+	double lastError;		// 上一次误差值
+	double errorIntegral;	// 误差积分值
+
+public:
+	void initial(double p, double i, double d, double target)
+	{
+		kp = p;
+		ki = i;
+		kd = d;
+		targetValue = target;
+		lastError = 0;
+		errorIntegral = 0;
+	}
+
+	double calculate(double input)
+	{
+		double error = targetValue - input;
+		double derivative = error - lastError;
+		errorIntegral += error;
+		lastError = error;
+		return kp * error + ki * errorIntegral + kd * derivative;
+	}
+};
+
+/*▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼*\
+█	车辆控制主程序，由ChatGPT自动生成助教完善								█
+█	样例代码仅供参考，请在下方设计实现您的算法								█
+\*▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲*/
+PIDController speedController;	//速度PID控制
+PIDController angleController;	//舵角PID控制
+PIDController offsetController;  //横向距离控制
+double lastTargetSpeed = 999.0;	//上一帧目标速度
+bool isFirstFrame = true;		//第一帧标志
+bool isCementRoad = true, flag = false;
+int timecounter = 0;
+
+static void userDriverSetParam(float* cmdAcc, float* cmdBrake, float* cmdSteer, int* cmdGear)
+{
+	/*▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼*\
+	█	检测路面																█
+	\*▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲*/
+	if (flag == false) timecounter++;
+	if (flag == false && timecounter == 60 && _speed < 30)
+	{
+		isCementRoad = false;
+		flag = true;
+	}
+
+
+
+	/*▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼*\
+	█	舵角控制																█
+	\*▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲*/
+	double targetAngleError = 0.0; //目标误差
+	double currentAngleError = atan2(_midline[5][0], _midline[5][1]); //当前误差
+	double kp = 0.0, ki = 0.0, kd = 0.0;
+
+	//计算前方是直道还是弯道
+	circle myCurve;
+	float minCruve = 500.0;
+	if (!isCementRoad)
+	{
+		for (int fStep = 0; fStep < 40; fStep++)
+		{
+			myCurve = getR(_midline[fStep][0], _midline[fStep][1], _midline[fStep + 10][0], _midline[fStep + 10][1], _midline[fStep + 20][0], _midline[fStep + 20][1]);
+			if (myCurve.r < minCruve)
+			{
+				minCruve = myCurve.r;
+			}
+		}
+	}
+	else
+	{
+		for (int fStep = 0; fStep < 50; fStep++)
+		{
+			myCurve = getR(_midline[fStep][0], _midline[fStep][1], _midline[fStep + 10][0], _midline[fStep + 10][1], _midline[fStep + 20][0], _midline[fStep + 20][1]);
+			if (myCurve.r < minCruve)
+			{
+				minCruve = myCurve.r;
+			}
+		}
+	}
+
+	//第一帧初始化舵角控制参数，清空积分器和微分器，因为控制目标为恒零，所以只需要初始化一次
+	if (isFirstFrame)
+	{
+		isFirstFrame = false;
+		angleController.initial(1.0, 0.0, 0.0, targetAngleError);
+	}
+
+	if (minCruve > 250)
+	{
+		kp = 0.5;
+		ki = 3;
+		kd = 0;
+	}
+	else if (minCruve > 120)
+	{
+		kp = 0.7;
+		ki = 3;
+		kd = 0;
+	}
+	else if (minCruve > 40)
+	{
+		kp = 0.9;
+		ki = 3;
+		kd = 0;
+	}
+	else
+	{
+		kp = 1.1;
+		ki = 3;
+		kd = 0;
+	}
+
+	angleController.initial(kp, ki, kd, targetAngleError);
+
+	//舵角PID控制
+	//*cmdSteer = constrain(-1.0, 1.0, angleController.calculate(currentAngleError));
+
+	/*▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼*\
+	█	横向距离控制																█
+	\*▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲*/
+	double targetOffsetError = 0.0; //目标误差
+	double currentOffsetError = _midline[1][0]; //当前误差
+	offsetController.initial(0.5, 3.0, 0.0, targetOffsetError);
+	*cmdSteer = constrain(-1.0, 1.0, constrain(-1.0, 1.0, angleController.calculate(currentAngleError)) + 0.2 * constrain(-1.0, 1.0, offsetController.calculate(currentOffsetError)));
+
+	/*▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼*\
+	█	速度控制																█
+	\*▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲*/
+	double targetSpeed;  //目标车速
+	double currentSpeed = _speed;	//当前误差
+
+
+	//设定目标速度，如果前方弯道就设定低，直道就设定高
+	if (isCementRoad)
+	{
+		if (minCruve > 450)
+			targetSpeed = 160;
+		else if (minCruve > 250)
+			targetSpeed = constrain(110, 160, minCruve * 0.2 + 59);
+		else if (minCruve > 100)
+			targetSpeed = constrain(90, 110, minCruve * 2 / 15 + 77);
+		else if (minCruve > 60)
+			targetSpeed = constrain(80, 90, minCruve * 0.25 + 67);
+		else if (minCruve > 20)
+			targetSpeed = constrain(65, 80, 0.375 * minCruve + 63);
+		else
+			targetSpeed = constrain(40, 65, minCruve * minCruve * (-0.046) + minCruve * 5.5 - 22.66);
+	}
+	else
+	{
+		if (minCruve > 450)
+			targetSpeed = 120;
+		else if (minCruve > 250)
+			targetSpeed = constrain(100, 120, minCruve * 0.1 + 85);
+		else if (minCruve > 100)
+			targetSpeed = constrain(85, 100, minCruve * 2 / 15 + 230 / 3);
+		else if (minCruve > 60)
+			targetSpeed = constrain(79, 85, minCruve * 0.25 + 65);
+		else if (minCruve > 20)
+			targetSpeed = constrain(60, 79, 0.375 * minCruve + 57.5);
+		else
+			targetSpeed = constrain(30, 60, minCruve * minCruve * (-0.046) + minCruve * 5.5 - 31.66);
+	}
+
+
+	//每当目标速度变化时初始化PID控制器，重设参数，清空积分器和微分器
+	if (targetSpeed != lastTargetSpeed)
+	{
+		speedController.initial(0.01, 0.5, 0.1, targetSpeed);
+		lastTargetSpeed = targetSpeed;
+	}
+
+	//根据当前速度和目标速度关系，控制油门刹车以改变速度
+	if (currentSpeed < targetSpeed)  //当前速度低于目标，需要加速
+	{
+		if (timecounter < 60)
+		{
+			*cmdAcc = 1.0;
+			*cmdBrake = 0.0;
+		}
+		else
+		{
+
+			if (_speed > 100 && abs(*cmdSteer) < 0.1)
+			{
+				//速度较快且舵角较小时，使用PID控制
+				*cmdAcc = constrain(0.0, 1.0, speedController.calculate(currentSpeed));
+			}
+			else if (_speed > 50 && abs(*cmdSteer) < 0.1)
+			{
+				//速度较慢且舵角较小时，限定油门
+				*cmdAcc = 0.65;
+			}
+			else
+			{
+				//除此之外的情况进一步限定油门
+				*cmdAcc = 0.2;
+			}
+			//加速情况下，刹车为0
+			*cmdBrake = 0;
+		}
+	}
+	else
+	{
+		//减速情况下，刹车
+		*cmdAcc = 0;
+		*cmdBrake = 0.43;
+	}
+
+	//更新档位
+	updateGear(cmdGear);
+
+	//窗口可视化
+	cls_visual.Fig2Y(1, 0, 300, 0, 500, 10, "Target V", targetSpeed, "Curvature", minCruve, "Current V", _speed);
+	cls_visual.Fig2Y(2, 0, 50, -90, 90, 1, "offsetError", currentOffsetError, "angleerror", currentAngleError);
+}
